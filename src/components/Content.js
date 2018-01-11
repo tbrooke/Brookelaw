69a7eff7c4b564445c21991a27e62041584dfab6
tbrooke <tom.brooke@gmail.com>
2018-01-10 20:14:55 -0500

    small change to test redeploy

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default ({ content, className }) => <div className={className}>{content}</div>;
 export const HTMLContent = ({ content, className }) => <div
   className={className}
